6fe7bbbb6ba56696c336d5750d2cde26209b49b9
RichardKim <hiddeneventor@gmail.com>
2022-03-08 20:09:12 +0900

    기본 객체 생성

---
+++ src/study/ch01/Student.java
@@ -0,0 +1,9 @@
+package study.ch01;
+
+public class Student {
+    int studentNumber;
+    String studentName;
+    int majorCode;
+    String majorName;
+    int grade;
+}
---
+++ src/study/ch01/UserInfo.java
@@ -0,0 +1,9 @@
+package study.ch01;
+
+public class UserInfo {
+    String userId;
+    String userPassword;
+    String userName;
+    String address;
+    int phoneNumber;
+}
